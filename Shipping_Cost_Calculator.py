14ce9a7b0b5799b7cd0b3cfe7232d08aa92cc4c6
Alfred Nobel <galidoalfred@gmail.com>
2025-08-20 02:35:33 +0800

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,4 @@
+ # Here is a new update by NobelGalido
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -10,3 +11,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+# Here is another update by NobelGalido
